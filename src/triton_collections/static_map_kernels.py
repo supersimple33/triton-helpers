8bad6ea2430e8c7e39eff4208ca61dfc5ed4e3d5
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2026-05-05 05:44:02 +0000

    feat: Added insert_key_linear function

--- src/triton_collections/static_map_kernels.py
+++ src/triton_collections/static_map_kernels.py
@@ -49,6 +49,41 @@ def find_or_insert_key_linear(
     tl.store(out_slots_ptr + offsets, slot_indices, mask=mask)
 
 @triton.jit
+def insert_key_linear(
+    keys_ptr,
+    in_keys_ptr,
+    out_slots_ptr,
+    out_inserted_ptr,
+    n_elements,
+    capacity,
+    empty_key,
+    MAX_PROBE: tl.constexpr,
+    BLOCK: tl.constexpr,
+):
+    """Inserts the given keys using linear probing. Skips keys that are already present."""
+    pid = tl.program_id(0)
+    offsets = pid * BLOCK + tl.arange(0, BLOCK)
+    mask = offsets < n_elements
+
+    in_keys = tl.load(in_keys_ptr + offsets, mask=mask, other=empty_key)
+    valid = mask & (in_keys != empty_key) # host should ensure that in_keys != empty_key
+    slots = in_keys % capacity
+
+    active = valid
+    slot_indices = tl.zeros((BLOCK,), dtype=slots.dtype) # safe since we report inserted
+    for i in range(MAX_PROBE):
+        idxs = (slots + i) % capacity
+
+        slot_keys = tl.load(keys_ptr + idxs, mask=active, other=empty_key)
+        hit = active & (slot_keys == empty_key)
+        slot_indices = tl.where(hit, idxs, slot_indices)
+        active = active & ~hit & (slot_keys != in_keys)
+    inserted = valid & (~active)
+
+    tl.store(out_inserted_ptr + offsets, inserted.to(out_inserted_ptr.dtype), mask=mask)
+    tl.store(out_slots_ptr + offsets, slot_indices, mask=mask)
+
+@triton.jit
 def find_key_linear(
     keys_ptr,
     in_keys_ptr,
